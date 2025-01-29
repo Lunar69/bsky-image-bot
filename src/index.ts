8e3feb6d5e9630e541b9c3379258fdd6eb93b190
Lunar <BattlequeenYume@protonmail.com>
2025-01-30 07:56:27 +1100

    ttttt

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import { postImage } from './clients/at'; // Importing from TypeScript file in clients folder
-import { getNextImage } from './images';   // Importing from TypeScript file in the same directory
+import { postImage } from './clients/at';
+import { getNextImage } from './images';
 import * as dotenv from 'dotenv';
-
 dotenv.config();
-
 
 // EDIT THIS!
 function postTextFromImageName(imageName: string): string {
-  // Remove the file extension and parse the date
-const imageName = imageName.replace('.jpg', '').split('-');
+  return 'UwU';
 }
 
 // EDIT THIS!
 function altTextFromImageName(imageName: string): string {
-return 'Image from ' + postTextFromImageName(imageName);
+  return 'Image from ' + postTextFromImageName(imageName);
+  // return '#ai #aiart #aiartwork #stablediffusionart #stablediffusion'
 }
 
+// Shouldn't have to edit this.
 async function main() {
   const { LAST_IMAGE_NAME: lastImageName } = process.env;
-
   const nextImage = await getNextImage({ lastImageName });
 
   console.log(nextImage.imageName);
@@ -27,10 +25,9 @@ async function main() {
     path: nextImage.absolutePath,
     text: postTextFromImageName(nextImage.imageName),
     altText: altTextFromImageName(nextImage.imageName),
+    // text: '#ai #aiart #aiartwork #stablediffusionart #stablediffusion',
+    // altText: 'aiart aiartwork stablediffusionart stablediffusion.',
   });
 }
 
-main().catch(error => {
-  console.error('Erro ao executar o script:', error);
-  process.exit(1);
-});
+main();
